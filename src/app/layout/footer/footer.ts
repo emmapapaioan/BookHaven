b6c9d17fe88fe282ddf2a5f75cb8bb739fef7aaa
Emma Papaioannou <emmapapaioannou@outlook.com>
2025-08-02 17:10:29 +0300

    chore(footer): make component standalone

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrls: ['./footer.scss']
 })
 export class Footer {
 
