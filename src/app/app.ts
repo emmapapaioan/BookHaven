467e133bb2a267665edf1d3c6db57d5f68eb5f1a
Emma Papaioannou <emmapapaioannou@outlook.com>
2025-08-02 17:07:36 +0300

    feat(layout): implement basic structure for header and sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
+import { Sidebar } from './layout/sidebar/sidebar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Sidebar],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
