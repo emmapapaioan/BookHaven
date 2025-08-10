14f41ac1e74659f1317f82511a162702725b3efa
Emma Papaioannou <emmapapaioannou@outlook.com>
2025-08-10 22:17:50 +0300

    feat(google-books): fetch books from API and render list in HTML

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,22 +1,37 @@
 import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 import { AuthManager } from '../../core/auth-manager';
 import { GoogleLogin } from '../google-login/google-login';
-import { MatIconModule } from '@angular/material/icon';
+import { GoogleBook } from '../../shared/interfaces/google-books-list';
+import { BehaviorSubject } from 'rxjs';
+import { GoogleBooksStore } from '../../core/google-books-store';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, GoogleLogin, MatIconModule],
+  imports: [CommonModule, GoogleLogin, MatIconModule, FormsModule, MatListModule],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
 export class Home {
   protected auth = inject(AuthManager);
+  private googleBooksStore = inject(GoogleBooksStore);
 
   // Use computed signal to know if user is logged in
   readonly isLoggedIn = computed(() => !!this.auth.user());
   readonly user = this.auth.user;
 
+  books$: BehaviorSubject<GoogleBook[]> = new BehaviorSubject<GoogleBook[]>([]);
+
+  constructor() { }
+
+  ngOnInit() {
+    this.books$ = this.googleBooksStore.googleBooks$;
+  }
 }
 
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,13 +1,20 @@
 import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+
 import { ThemeManager } from '../../core/theme-manager';
 import { AuthManager } from '../../core/auth-manager';
 import { GoogleLogin } from "../../features/google-login/google-login";
+import { GoogleBooksApi } from '../../core/google-books-api';
+import { GoogleBook } from '../../shared/interfaces/google-books-list';
+import { GoogleBooksStore } from '../../core/google-books-store';
+import { finalize } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-header',
@@ -18,7 +25,9 @@ import { GoogleLogin } from "../../features/google-login/google-login";
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    GoogleLogin
+    GoogleLogin,
+    FormsModule,
+    MatProgressSpinnerModule
 ],
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
@@ -26,10 +35,32 @@ import { GoogleLogin } from "../../features/google-login/google-login";
 export class Header {
   protected readonly auth = inject(AuthManager);
   private themeManager = inject(ThemeManager);
+  private googleBooksStore = inject(GoogleBooksStore);
 
   // Use computed signal to know if user is logged in
   readonly isLoggedIn = computed(() => !!this.auth.user());
   readonly user = this.auth.user;
+
+  private googleBooksApi = inject(GoogleBooksApi);
+  query: string = '';
+  googleBooks: GoogleBook[] = [];
+  isLoading: boolean = false;
+
+  searchBooks() {
+    this.isLoading = true;
+
+    this.googleBooksApi.searchBooks(this.query)
+      .subscribe({
+        next: (res) => {
+          this.googleBooksStore.setGoogleBooks(res?.items ?? []);
+          this.isLoading = false;
+        },
+        error: (error) => {
+          this.isLoading = false;
+          console.log(error)
+        },
+      });
+  }
 
   logout() {
     this.auth.logout();
