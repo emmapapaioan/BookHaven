8dab7b9694af88dd6d0f5720e8f80b03ff6c1ae3
Emma Papaioannou <emmapapaioannou@outlook.com>
2025-09-07 17:51:08 +0300

    Add routerLink for active links

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -2,19 +2,16 @@ import { Component } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule, RouterModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss']
 })
 export class Sidebar {
-  constructor(private router: Router) { }
+  constructor() { }
 
-  goToSearchBooks() {
-    this.router.navigate(['/search-books']);
-  }
 }
