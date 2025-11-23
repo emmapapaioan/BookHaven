5e7d10f408fde1749332de8aeda07b434e97cf25
Emma Papaioannou <emmapapaioannou@outlook.com>
2025-11-23 11:08:52 +0200

    temporarily add redirect urls for info and preview

--- src/app/features/search-books/book-details/book-details.ts
+++ src/app/features/search-books/book-details/book-details.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectionStrategy, Component, Input, SimpleChange } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Inject, Input } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { GoogleBook } from '../../../shared/interfaces/google-books';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatTooltipModule],
+  imports: [MatCardModule, MatButtonModule, MatTooltipModule, MatSnackBarModule],
   templateUrl: './book-details.html',
   styleUrls: ['./book-details.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -19,4 +20,23 @@ export class BookDetails {
   @Input() viewMode: 'default' | 'withImage' | 'list' = 'default';
 
   defaultCover = 'images/default-cover.png';
+
+  private snackBar = inject(MatSnackBar);
+
+  showInfo(infoLink: string | undefined): void {
+    if (!infoLink) {
+      this.snackBar.open('No info link available', 'OK', { duration: 2500 });
+      return;
+    }
+    window.open(infoLink, '_blank');
+  }
+
+  showPreview(previewLink: string | undefined): void {
+    if (!previewLink) {
+      this.snackBar.open('No preview available', 'OK', { duration: 2500 });
+      return;
+    }
+    window.open(previewLink, '_blank');
+  }
+
 }
